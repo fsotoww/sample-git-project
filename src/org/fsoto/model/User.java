0b4e0521e94e12a2603592bf190b5d693ee9186f
Felipe Soto <fsoto@wovenware.com>
2021-11-26 16:01:36 -0400

    Add getter and setter for age

--- src/org/fsoto/model/User.java
+++ src/org/fsoto/model/User.java
@@ -6,6 +6,14 @@ public class User {
 	
 	private int age;
 
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
 	public String getName() {
 		return name;
 	}
